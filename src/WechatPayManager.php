0b6d0dd79158d8fe060941b2652dfd658bd39055
Pham Nguyen <nguyen.pham@axonactive.vn>
2017-03-14 16:32:59 +0700

    add libs

--- src/WechatPayManager.php
+++ src/WechatPayManager.php
@@ -1,11 +1,26 @@
 <?php
 
 namespace Lod\WechatPay;
-
 use InvalidArgumentException;
+
 use Illuminate\Support\Manager;
 
-class WeChatPayManager extends Manager
-{
+class WechatPayManager extends Manager {
 
+
+    public static function unifiedOrder(){
+        return Libs\WxPayApi::unifiedOrder();
+    }
+
+    /**
+    * Get the default driver name.
+    *
+    * @throws \InvalidArgumentException
+    *
+    * @return string
+    */
+    public function getDefaultDriver()
+    {
+        throw new InvalidArgumentException('No wechatpay driver was specified.');
+    }
 }
--- src/WechatPayServiceProvider.php
+++ src/WechatPayServiceProvider.php
@@ -4,23 +4,46 @@ namespace Lod\WechatPay;
 
 use Illuminate\Support\ServiceProvider;
 
-class WeChatPayServiceProvider extends ServiceProvider
-{
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
+
+class WechatPayServiceProvider extends ServiceProvider {
 
     /**
-     * Register the service provider.
-     *
-     * @return void
-     */
+    * Indicates if loading of the provider is deferred.
+    *
+    * @var bool
+    */
+    protected $defer = true;
+    //
+    // /**
+    // * Bootstrap the application events.
+    // *
+    // * @return void
+    // */
+    // public function boot()
+    // {
+    //     $this->package('Lod/WechatPay');
+    // }
+
+    /**
+    * Register the service provider.
+    *
+    * @return void
+    */
     public function register()
     {
-        $this->package('lod\wechatpay');
+        $this->app->singleton('Lod\WechatPay\WechatPayManager',  function($app) {
+            return new WechatPayManager($app);
+        });
+    }
+
+    /**
+    * Get the services provided by the provider.
+    *
+    * @return array
+    */
+    public function provides()
+    {
+        return array('Lod\WechatPay\WechatPayManager');
     }
 
 }
